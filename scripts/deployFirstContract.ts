54ec41a287bd1fade56ebaecab1159137666ad7d
Богданов Семен <twist.tracer@gmail.com>
2025-12-29 22:09:10 +0200

    <master> first contract by official guide

--- scripts/deployFirstContract.ts
+++ scripts/deployFirstContract.ts
@@ -3,11 +3,14 @@ import { FirstContract } from '../wrappers/FirstContract';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const firstContract = provider.open(FirstContract.createFromConfig({}, await compile('FirstContract')));
+    const firstContract = provider.open(
+        FirstContract.createFromConfig(
+            { counter: Math.floor(Math.random() * 10000000) },
+            await compile('FirstContract'),
+        ),
+    );
 
     await firstContract.sendDeploy(provider.sender(), toNano('0.05'));
 
     await provider.waitForDeploy(firstContract.address);
-
-    // run methods on `firstContract`
 }
--- wrappers/FirstContract.ts
+++ wrappers/FirstContract.ts
@@ -1,32 +1,13 @@
-import {
-    Address,
-    beginCell,
-    Cell,
-    Contract,
-    ContractABI,
-    contractAddress,
-    ContractProvider,
-    Sender,
-    SendMode
-} from '@ton/core';
-
-export type FirstContractConfig = {};
-
-export function firstContractConfigToCell(config: FirstContractConfig): Cell {
-    return beginCell().endCell();
-}
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
 export class FirstContract implements Contract {
-    abi: ContractABI = { name: 'FirstContract' }
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
-
-    static createFromAddress(address: Address) {
-        return new FirstContract(address);
-    }
-
-    static createFromConfig(config: FirstContractConfig, code: Cell, workchain = 0) {
-        const data = firstContractConfigToCell(config);
+    static createFromConfig(config: { counter: number }, code: Cell, workchain = 0) {
+        const data = beginCell().storeUint(config.counter, 64).endCell();
         const init = { code, data };
         return new FirstContract(contractAddress(workchain, init), init);
     }
@@ -35,7 +16,6 @@ export class FirstContract implements Contract {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
         });
     }
 }
